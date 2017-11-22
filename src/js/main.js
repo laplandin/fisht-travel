52199ccca988472712ab1f6c471119d6ec629d39
laplandin <deniskakhv@gmail.com>
2017-11-23 00:31:52 +0300

    rewrite route-animate lib

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,21 @@
+var globalState = {
+  "page-1": "index.html",
+  "page-2": "tours.html"
+};
 $(window).on('load', function() {
-   console.log('jquery + scripts builds Ok');
+
+
 });
+
+function loadPage (href) {
+  $.ajax({
+    type: "GET",
+    url: "tours.tmp.html",
+    data: {},
+    success: function(response) {
+      $("#pt-main").append('<div class="pt-page">'+ response +'</div>');
+      PageTransitions.init();
+      PageTransitions.nextPage({animation: 3, showPage: 1})
+    }
+  })
+}
